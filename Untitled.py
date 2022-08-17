d89dba610eb67966c74d8acc271947f9f95ed1c2
Rajesh250595 <111446381+REDRAJESH@users.noreply.github.com>
2022-08-17 23:47:07 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+rajesh_Atm =int(input("Please enter your pin number"))
+if (rajesh_Atm == 4567):
+    print("hi rajesh Please Tellme the amount you want to take")
+else:
+    print("you are not rajesh and the pin is wrong please insert correct pin")
+    
+
+
+# In[ ]:
+
+
+
+
