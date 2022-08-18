9dfc3738ff666b74677602b565ef94d2d161a029
Rajesh250595 <111446381+REDRAJESH@users.noreply.github.com>
2022-08-18 22:16:16 +0530

    18-08-2022 class

---
+++ Startup class.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+
+
+
+# In[20]:
+
+
+
+
+
+# In[10]:
+
+
+
+
+
+# In[11]:
+
+
+
+
+
+# In[13]:
+
+
+
+
+
+# In[14]:
+
+
+
+
+
+# In[21]:
+
+
+#this is int cannot used to variable
+1d=10
+print (1d)
+
+
+# In[23]:
+
+
+#this is Specla Charater first letter typeing in cannot used to variable
+&q=10
+print (&q)
+
+
+# In[24]:
+
+
+#this is mulitfuction opertor cannot crt to variable
+a+2b=20
+print (a+2b)
+
+
+# In[26]:
+
+
+#this is key word not used to veriable programming run and output is wrong
+a=rajesh
+sum = rajesh
+print (sum)
+
+
+# In[27]:
+
+
+#this is pascel charater cannot used to veriable
+get_ipython().system('=10')
+print (!)
+
+
+# In[31]:
+
+
+# single ling to two verialble and two value cannot access
+   a=10,b=10
+print (a,b)    
+   
+
+
+# In[1]:
+
+
+#this is keyword and specel chareter cannot used veriabl
+continue = 10
+print (continue)
+
+
+# In[4]:
+
+
+#sigle line for used to veriable and value
+a,b=10,20
+sum = a+b
+print (sum)
+
+
+# In[7]:
+
+
+#this is value in specel chareter used in cannot run the syntax
+   a = -90-
+print (a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
